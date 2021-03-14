954e722f144c29f305245d52f39d1bb1e991ccd9
h2608011009 <2608011009@qq.com>
2021-03-14 22:30:23 +0800

    1.20

---
+++ test_1_20/test_1_20/源.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+
+//int main()
+//{
+//	char arr1[] = "bit";
+//	char arr2[20] = "*************";
+//		strcpy(arr2, arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
+//void Swap(int *x, int *y)
+//{
+//	int tmp = 0;
+//	tmp = *x;
+//	*x = *y;
+//		*y = tmp;
+//
+//}
+//
+//int main()
+//{
+// 	int a = 10;
+//	int b = 20;
+//
+//
+//	Swap(&a, &b);
+//	return 0;
+//}
+
+
+//void sushu(int x)
+//{
+//
+//	for ()
+//	
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		//ÅÐ¶ÏiÊÇ·ñÎªËØÊý
+//		if (sushu(i) == 1)
+//			printf("%d", i);
+//	}
+//	return 0;
+//}
+
+int main()
+{
+	int x = -1;
+	x << 3;
+	printf("%d", x);
+	return 0;
+}
